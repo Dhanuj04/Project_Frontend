b7107b485e5ff0fa57f0debe039c326aa6d2f091
Dhanuj R <83819952+Dhanuj04@users.noreply.github.com>
2023-11-15 10:40:59 +0530

    Add files via upload

--- UserDashboard.js
+++ UserDashboard.js
@@ -1,11 +1,194 @@
+// UserDashboard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PolicyIcon from '@material-ui/icons/Policy';
+import FeedbackIcon from '@material-ui/icons/Feedback';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
 
-function UserDashboard() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  appBar: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  welcomeCard: {
+    marginBottom: theme.spacing(2),
+  },
+  welcomeImage: {
+    height: 150, // Adjust the height of the image as needed
+    backgroundSize: 'contain',
+  },
+  bottomAppBar: {
+    top: 'auto',
+    bottom: 0,
+    backgroundColor: theme.palette.primary.main,
+    marginTop: theme.spacing(2),
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  content: {
+    display: 'flex',
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  leftColumn: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    marginRight: theme.spacing(2),
+    marginLeft: -theme.spacing(2),
+    padding: theme.spacing(2),
+    paddingBottom: theme.spacing(0),
+    paddingTop: theme.spacing(5),
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: theme.spacing(8),
+    marginTop: theme.spacing(0),
+  },
+  rightColumn: {
+    flex: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', // Center align text horizontally
+    justifyContent: 'center', // Center align text vertically
+  },
+  icon: {
+    fontSize: 40,
+    marginRight: theme.spacing(1),
+  },
+  logoutContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logoutButton: {
+    color: 'inherit',
+    textTransform: 'none', // Prevent text transformation in the button
+  },
+  text: {
+    fontSize: 20,
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing(4),
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  logoutButton: {
+    color: theme.palette.secondary.main,
+  },
+}));
+
+const UserDashboard = () => {
+  const classes = useStyles();
+
+  const handleLogout = () => {
+    // Add your logout logic here
+    console.log('Logout clicked');
+  };
+
   return (
-    <div>
-      <h1>Welcome to Your Dashboard</h1>
+    <div className={classes.root}>
+        <AppBar position="static" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" color="inherit">
+            Insurance Management
+          </Typography>
+          <div className={classes.logoutContainer}>
+            <Button className={classes.logoutButton} onClick={handleLogout}>
+              <ExitToAppIcon />
+              <Typography variant="h6" color="inherit">
+                Logout
+              </Typography>
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      <div className={classes.content}>
+        <div className={classes.leftColumn}>
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <DashboardIcon className={classes.icon} />
+              Dashboard
+            </Typography>
+          </Link>
+
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <PolicyIcon className={classes.icon} />
+              Policy Categories
+            </Typography>
+          </Link>
+
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <PolicyIcon className={classes.icon} />
+              Available Policy
+            </Typography>
+          </Link>
+
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <PolicyIcon className={classes.icon} />
+              Applied Policy
+            </Typography>
+          </Link>
+
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <PolicyIcon className={classes.icon} />
+              Apply Policy
+            </Typography>
+          </Link>
+
+          <Link to="/register" className={classes.text}>
+            <Typography variant="h6" className={classes.text}>
+              <FeedbackIcon className={classes.icon} />
+              Feedback
+            </Typography>
+          </Link>
+        </div>
+
+        <div className={classes.rightColumn}>
+          {/* Welcome Card */}
+          <Card className={classes.welcomeCard}>
+            <CardMedia
+              className={classes.welcomeImage}
+              image="https://aryasamaj.com/wp-content/uploads/2021/04/namaste.png" // Replace with the actual path to your image
+              title="Welcome Image"
+            />
+            <CardContent>
+              <Typography variant="h6" align="center">Welcome to Insurance Management</Typography>
+              <Typography variant="body2" align="center">
+              An Insurance Management System (IMS) is a sophisticated software solution designed to revolutionize and streamline the complex operations within the insurance industry. Serving as the backbone of insurance companies, the IMS consolidates and automates key processes, ranging from policy creation and issuance to claims processing and customer management. This comprehensive system facilitates the seamless administration of insurance policies, allowing companies to efficiently assess risks, generate quotes, and manage premium calculations. With features like underwriting support and agent performance monitoring, the IMS empowers insurance providers to make informed decisions and enhance overall operational efficiency. Robust billing and premium management capabilities, coupled with secure document storage and compliance reporting, ensure financial accuracy, regulatory adherence, and data security. By integrating seamlessly with external systems and offering robust reporting and analytics tools, the IMS becomes a pivotal tool for insurance companies to adapt to evolving market demands, provide superior customer service, and maintain a competitive edge in the dynamic landscape of the insurance sector
+              </Typography>
+            </CardContent>
+          </Card>
+
+          {/* ... (any additional content in the right column) */}
+        </div>
+      </div>
+
+      {/* Bottom AppBar */}
+      <AppBar position="fixed" className={classes.bottomAppBar}>
+        <Toolbar className={classes.toolbar}>
+          {/* Add content for the bottom bar if needed */}
+        </Toolbar>
+      </AppBar>
     </div>
   );
-}
+};
 
 export default UserDashboard;
